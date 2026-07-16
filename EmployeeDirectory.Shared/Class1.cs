725d32b0253b0c725905316a69db8b5c7ee65771
David Rossi <DRossi@CheckWriters.com>
2026-07-16 13:57:28 -0400

    Initial solution structure

---
+++ EmployeeDirectory.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeeDirectory.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EmployeeDirectory.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeDirectory.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
