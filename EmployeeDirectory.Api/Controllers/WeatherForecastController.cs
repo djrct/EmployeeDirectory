d88caafe2241368b2b62be38b9a162d577b81798
djrct <Drossi@checkwriters.com>
2026-07-18 18:18:06 -0400

    Test bad build in PR

--- EmployeeDirectory.Api/Controllers/WeatherForecastController.cs
+++ EmployeeDirectory.Api/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace EmployeeDirectory.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : ControllerBase1
     {
         private static readonly string[] Summaries = new[]
         {
